3d4ff2ae895bee8da65dbca470f2192b5684df68
Joost Kiens <me@joostkiens.com>
2019-12-16 15:29:18 +0100

    add presets

--- src/domain/Form.js
+++ src/domain/Form.js
@@ -4,6 +4,7 @@ import styles from './Form.css'
 export function Form({ onSubmit, formState, layoutClassName }) {
   return (
     <form {...{ onSubmit }} className={cx(styles.component, layoutClassName)}>
+      <Presets layoutClassName={styles.presets} {...{ formState }} />
       <InputNumberGroup
         name='mass' label='Mass' layoutClassName={styles.inputGroup}
         min={0} max={20} step={0.1} {...{ formState }}
@@ -84,5 +85,33 @@ function CopyToClipboard({ layoutClassName, config }) {
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM192 40c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm144 418c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V118c0-3.3 2.7-6 6-6h42v36c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-36h42c3.3 0 6 2.7 6 6z" /></svg>
     </button>
+  )
+}
+
+function Presets({ formState, layoutClassName }) {
+  const [formValues, setFormValues] = React.useMemo(() => formState, [formState])
+  const handleChange = React.useCallback(
+    ({ target: { value } }) => { value && setFormValues(x => ({ ...x, ...JSON.parse(value) })) },
+    [setFormValues]
+  )
+
+  const value = React.useMemo(
+    () => {
+      const { mass, tension, friction } = formValues
+      return JSON.stringify({ mass, tension, friction })
+    },
+    [formValues]
+  )
+
+  return (
+    <select onChange={handleChange} className={cx(styles.componentPresets, layoutClassName)} {...{ value }}>
+      <option value=''>Presets</option>
+      <option value='{"mass":1,"tension":170,"friction":26}'>Default</option>
+      <option value='{"mass":1,"tension":120,"friction":14}'>Gentle</option>
+      <option value='{"mass":1,"tension":180,"friction":12}'>Wobbly</option>
+      <option value='{"mass":1,"tension":210,"friction":20}'>Stiff</option>
+      <option value='{"mass":1,"tension":280,"friction":60}'>Slow</option>
+      <option value='{"mass":1,"tension":280,"friction":120}'>Molasses</option>
+    </select>
   )
 }
