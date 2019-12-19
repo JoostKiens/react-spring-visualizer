1e0268be594509cec93913763713c6096c091426
Joost Kiens <me@joostkiens.com>
2019-12-19 18:13:00 +0100

    change meta colors to match theme

--- src/index.html.js
+++ src/index.html.js
@@ -20,11 +20,11 @@ export default (
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
       <link rel="manifest" href="/static/site.webmanifest" />
-      <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#ffff00" />
+      <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#ffd369" />
       <link rel="shortcut icon" href="/static/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#ffff00" />
+      <meta name="msapplication-TileColor" content="#ffd369" />
       <meta name="msapplication-config" content="/static/browserconfig.xml" />
-      <meta name="theme-color" content="#ffff00" />
+      <meta name="theme-color" content="#ffd369" />
     </head>
     <body>
       <App />
