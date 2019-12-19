57de1aa5844529bf22de12a3a5f62d4decb88503
Joost Kiens <me@joostkiens.com>
2019-12-19 16:09:49 +0100

    create static site

--- src/index.html.js
+++ src/index.html.js
@@ -5,42 +5,29 @@ import javascript from '@kaliber/build/lib/javascript'
 import polyfill from '@kaliber/build/lib/polyfill'
 import App from '/domain/App?universal'
 
-Index.routes = {
-  match(location) {
-    const path = location.pathname
-    if (path === '/') return { status: 200 }
-    else return { status: 404, data: { notFound: true } }
-  }
-}
-
-export default function Index({ location, data }) {
-  return (
-    <html lang='nl'>
-      <head>
-        <meta charSet='utf-8' />
-        <title>React Spring visualizer</title>
-        <meta name='description' content='' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,700&display=swap' rel='stylesheet' />
-        {stylesheet}
-        {polyfill(['default', 'es2015', 'es2016', 'es2017', 'es2018', 'es2019'])}
-        {javascript}
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-        <link rel="manifest" href="/static/site.webmanifest" />
-        <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#ffff00" />
-        <link rel="shortcut icon" href="/static/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#ffff00" />
-        <meta name="msapplication-config" content="/static/browserconfig.xml" />
-        <meta name="theme-color" content="#ffff00" />
-      </head>
-      <body>
-        {data && data.notFound
-          ? 'Not found'
-          : <App />
-        }
-      </body>
-    </html>
-  )
-}
+export default (
+  <html lang='en'>
+    <head>
+      <meta charSet='utf-8' />
+      <title>React Spring visualizer</title>
+      <meta name='description' content='' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,700&display=swap' rel='stylesheet' />
+      {stylesheet}
+      {polyfill(['default', 'es2015', 'es2016', 'es2017', 'es2018', 'es2019'])}
+      {javascript}
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+      <link rel="manifest" href="/static/site.webmanifest" />
+      <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#ffff00" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#ffff00" />
+      <meta name="msapplication-config" content="/static/browserconfig.xml" />
+      <meta name="theme-color" content="#ffff00" />
+    </head>
+    <body>
+      <App />
+    </body>
+  </html>
+)
