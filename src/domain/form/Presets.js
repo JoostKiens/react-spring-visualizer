f9bdc7d8449eac1fc3659f1356d86565b4983f0f
Joost Kiens <me@joostkiens.com>
2019-12-22 14:41:22 +0100

    refactor presets

--- src/domain/form/Presets.js
+++ src/domain/form/Presets.js
@@ -1,6 +1,16 @@
 import { SrOnly } from '/machinery/SrOnly'
 import styles from './Presets.css'
 
+const presets = [
+  ['', 'Presets'],
+  [JSON.stringify({ mass: 1, tension :170, friction :26 }), 'Default'],
+  [JSON.stringify({ mass: 1, tension :120, friction: 14 }), 'Gentle'],
+  [JSON.stringify({ mass: 1, tension :180, friction: 12 }), 'Wobbly'],
+  [JSON.stringify({ mass: 1, tension :210, friction: 20 }), 'Stiff'],
+  [JSON.stringify({ mass: 1, tension :280, friction: 60 }), 'Slow'],
+  [JSON.stringify({ mass: 1, tension :280, friction: 120 }), 'Molasses']
+]
+
 export function Presets({ formState, layoutClassName, onChange }) {
   const [formValues, setFormValues] = React.useMemo(() => formState, [formState])
   const handleChange = React.useCallback(
@@ -13,7 +23,7 @@ export function Presets({ formState, layoutClassName, onChange }) {
     [setFormValues, onChange]
   )
 
-  const value = React.useMemo(
+  const currentValue = React.useMemo(
     () => {
       const { mass, tension, friction } = formValues
       return JSON.stringify({ mass, tension, friction })
@@ -24,14 +34,8 @@ export function Presets({ formState, layoutClassName, onChange }) {
   return (
     <div className={layoutClassName}>
       <SrOnly><label htmlFor='presets'>Choose a preset</label></SrOnly>
-      <select onChange={handleChange} className={styles.component} id='presets' {...{ value }}>
-        <option value=''>Presets</option>
-        <option value='{"mass":1,"tension":170,"friction":26}'>Default</option>
-        <option value='{"mass":1,"tension":120,"friction":14}'>Gentle</option>
-        <option value='{"mass":1,"tension":180,"friction":12}'>Wobbly</option>
-        <option value='{"mass":1,"tension":210,"friction":20}'>Stiff</option>
-        <option value='{"mass":1,"tension":280,"friction":60}'>Slow</option>
-        <option value='{"mass":1,"tension":280,"friction":120}'>Molasses</option>
+      <select onChange={handleChange} className={styles.component} id='presets' value={currentValue}>
+        {presets.map(([value, children]) => <option key={value} {...{ value, children }} />)}
       </select>
     </div>
   )
