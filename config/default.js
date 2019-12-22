fe5686429c3bd54546011ce95c257334585f6370
Joost Kiens <me@joostkiens.com>
2019-12-22 15:34:33 +0100

    transpile @kaliber libs

--- config/default.js
+++ config/default.js
@@ -1,4 +1,7 @@
 module.exports = {
+  kaliber: {
+    compileWithBabel: [/@kaliber\//]
+  }
   /*
     Warning: do not use this to set values that differ in each environment,
     only use this for configuration that is the same across all config environments
